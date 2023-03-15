bdb272a8534f6a5cf77df45117d7b69c592018c4
Shouhei Hayakawa <s-hayakawa@esm.co.jp>
2023-03-15 21:02:25 +0900

    change structure

--- graphics/main.ts
+++ graphics/main.ts
@@ -1,5 +1,5 @@
-import './app.css'
-import App from './App.svelte'
+import '~/browser/graphics/app.css'
+import App from '~/browser/graphics/App.svelte'
 
 const app = new App({
   target: document.getElementById('app'),
