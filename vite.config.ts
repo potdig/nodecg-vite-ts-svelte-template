beb4bb95206a5346ae8b806fbe542b1c8ed52c88
Shouhei Hayakawa <s-hayakawa@esm.co.jp>
2023-03-15 21:05:04 +0900

    setup building environment for graphics

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,14 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: '/bundles/bundle-name/',
+  build: {
+    rollupOptions: {
+      input: {
+        'graphics/index': resolve(__dirname, 'graphics/index.html'),
+      },
+    },
+  },
   resolve: {
     alias: {
       '~': resolve(__dirname, 'src'),
